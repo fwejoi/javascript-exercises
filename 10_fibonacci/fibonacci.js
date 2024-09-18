6567e88ccfa871f53ea2ed06ac1110c58804ee7d
fwejoi <137739807+fwejoi@users.noreply.github.com>
2024-09-18 17:56:10 +0800

    fibonaacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number=parseInt(number);
+    if(number===0){
+        return 0
+    }else if(number<0){
+        return "OOPS";
+    }
+    else if(number===1){
+        return 1;
+    }else{
+        return fibonacci(number-2)+fibonacci(number-1);
+    }
 };
 
 // Do not edit below this line
