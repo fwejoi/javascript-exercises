7dfe1e10b53cf608cdc8b54385fae1590fa5ea84
fwejoi <137739807+fwejoi@users.noreply.github.com>
2024-09-18 18:53:26 +0800

    findTheOldset

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currentPerson)=>{
+        const currentYear=(new Date()).getFullYear();
 
+        const oldestAge =(oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge =(currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+
+        return currentAge>oldestAge?currentPerson:oldest;
+    })
 };
 
 // Do not edit below this line
