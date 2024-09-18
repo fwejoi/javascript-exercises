ba65c978762c4add0fb1bbbb98e098eaf6cf41a8
fwejoi <137739807+fwejoi@users.noreply.github.com>
2024-09-18 15:24:18 +0800

    stringrepeat

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if(num<0) return 'ERROR';
+    return string.repeat(num);//repeat为js重string内置函数
 };
 
 // Do not edit below this line
