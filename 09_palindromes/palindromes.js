3d26ba5d1c4ae9b9a14d1f130bf2e85b38c88f45
fwejoi <137739807+fwejoi@users.noreply.github.com>
2024-09-18 17:49:03 +0800

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+    const reverseStr=cleanedStr.split('').reverse().join('');
+
+    return cleanedStr===reverseStr;
 };
 
 // Do not edit below this line
